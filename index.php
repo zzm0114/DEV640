9d258f563be40b7c205cbf3fa9fb5546ec6481ff
Yuntong Lin <yuntong.lin@gmail.com>
2024-05-25 16:37:17 -0700

    update layout and logo

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
   session_start();
   require_once 'header.php';
-  echo "<div class='center'>Welcome to Robin's Nest,";
+  echo "<div class='center'>Welcome to TeamingUp,";
   if ($loggedin) echo " $user, you are logged in";
   else           echo ' please sign up or log in';
   echo <<<_END
       </div><br>
       </div>
       <div data-role="footer">
-        <h4>Web App from <i><a href='http://lpmj.net/5thedition'
-        target='_blank'>Learning PHP MySQL & JavaScript Ed. 5</a></i></h4>
+        <h4>Web-Based Social Networking App by Team Red</h4>
+        <p class="author">Authors: Zheming, Zhen, Yuntong, Tanh</p>
       </div>
     </body>
    </html>
